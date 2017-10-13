69d3603d29731eaf62bb6042835ec3fcf5dd926a
mishrasunny174 <mishrasunny174@gmail.com>
2017-10-13 22:34:46 +0530

    made practical1.cpp object oriented

--- practical1.cpp
+++ practical1.cpp
@@ -5,22 +5,55 @@
 #include <iostream>
 #include <cstdlib>
 
+// class for digits of a number
+class DigitsOfNum
+{
+	int num;
+public:
+	DigitsOfNum(int);
+	int getSumOfDigits(); //return sum of digits of a number
+	int getProductOfDigits(); //return product of digits of a number
+
+};
+
 using namespace std;
 
 int main()
 {
-    int num,sum=0,product=1;
+    int num;
     cout<<"enter a number: ";
     cin>>num;
-    while(num)
-    {
-        sum+=num%10;
-        product*=num%10;
-        num/=10;
-    }
-    cout<<"sum of the digits of number is: "<<sum<<endl;
-    cout<<"product of the digits of number is: "<<product<<endl;
+    DigitsOfNum D(num);
+    cout<<"sum of the digits of number is: "<<D.getSumOfDigits()<<endl;
+    cout<<"product of the digits of number is: "<<D.getProductOfDigits()<<endl;
     cin.ignore();
     cin.get();
     return 0;
+}
+
+DigitsOfNum::DigitsOfNum(int num)
+{
+	this->num=num;
+}
+
+int DigitsOfNum::getSumOfDigits()
+{
+	int sum=0,temp=this->num;
+	while(temp)
+    	{
+        	sum+=temp%10;
+        	temp/=10;
+    	}
+	return sum;
+}
+
+int DigitsOfNum::getProductOfDigits()
+{
+	int product=1,temp=this->num;
+	while(temp)
+	{
+		product*=temp%10;
+		temp/=10;
+	}
+	return product;
 }
